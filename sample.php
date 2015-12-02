0b0c1cdbd3886907f8a77dc03638c079988d00f7
shfeat <shfeat@gmail.com>
2015-12-02 15:07:15 +0800

    init

--- sample.php
+++ sample.php
@@ -1,11 +1,13 @@
 <?php
 require __DIR__.'/init.php';
 
-$site = 'atfield';
-$spider = new App\Libs\Spider($site);
-$spider->run();
-//$spider->run(1, 10);
-echo 'spider done';
-
 //php simple.php atfield
-//nohup php simple.php atfield > images.log &
+//nohup php simple.php atfield 1 10 &
+
+$site = isset($argv[1])? $argv[1] : 'atfield';
+$start = isset($argv[2])? $argv[2] : 0;
+$end = isset($argv[3])? $argv[3] : 0;
+
+$spider = new App\Libs\Spider($site);
+$spider->run($start, $end);
+echo 'spider done';
